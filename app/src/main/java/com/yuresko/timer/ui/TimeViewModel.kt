f0b458857eab527c3d4778f367529e9298a54793
nanemo <nanemo@mashapp.me>
2023-07-12 15:05:41 +0300

    Initial commit

---
+++ app/src/main/java/com/yuresko/timer/ui/TimeViewModel.kt
@@ -0,0 +1,14 @@
+package com.yuresko.timer.ui
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+
+class TimeViewModel : ViewModel() {
+
+    init {
+        viewModelScope.launch {
+
+        }
+    }
+}
